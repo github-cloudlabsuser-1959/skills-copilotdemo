97c94e50bfc731b91e8d35ce92cc000a218a93c3
github-cloudlabsuser-1959 <github_cloudlabsuser_1959@cloudlabsaiuser.com>
2024-06-12 19:28:46 +0000

    Copilot second commit

---
+++ prime.py
@@ -0,0 +1,10 @@
+def prime(n): 
+    """Returns True if n is prime, False otherwise."""
+    if n == 2:
+        return True
+    if n < 2 or n % 2 == 0:
+        return False
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
